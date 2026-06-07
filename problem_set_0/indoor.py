2f164fdfed0df1090d192dd33c614af203a9ce37
Poorvajaa S <poorvajaa.16@gmail.com>
2026-06-07 12:45:15 +0530

    Implement solution for the problem indoor.py

---
+++ problem_set_0/indoor.py
@@ -0,0 +1,11 @@
+'''
+
+In a file called indoor.py, implement a program in Python that prompts the user for input and then
+ outputs that same input in lowercase. Punctuation and whitespace should be outputted unchanged.
+
+'''
+
+a = input("Enter a sentence: ")
+print("Your output in lower case is: " )
+b = a.lower()
+print(b)
