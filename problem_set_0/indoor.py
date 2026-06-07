51513b1482dfe50ed58518b0da886d1185bea957
Poorvajaa S <poorvajaa.16@gmail.com>
2026-06-07 13:09:43 +0530

    Implement solution for problem playback.py

--- problem_set_0/indoor.py
+++ problem_set_0/indoor.py
@@ -1,7 +1,8 @@
 '''
+QUESTION:
 
 In a file called indoor.py, implement a program in Python that prompts the user for input and then
- outputs that same input in lowercase. Punctuation and whitespace should be outputted unchanged.
+outputs that same input in lowercase. Punctuation and whitespace should be outputted unchanged.
 
 '''
 
---
+++ problem_set_0/playback.py
@@ -0,0 +1,11 @@
+'''
+QUESTION:
+
+In a file called playback.py, implement a program in Python that prompts the user for 
+input and then outputs that same input, replacing each space with ... (i.e., three periods).
+
+'''
+
+s = input("Enter a sentence: ")
+print("The output is:")
+print(s.replace(" ", "..."))
