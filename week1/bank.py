e204d4333ff045dce1298826cdd69e8fe0a70d37
Poorvajaa <poorvajaa.16@gmail.com>
2026-08-19 10:21:12 +0530

    Implement bank greeting program

---
+++ week1/bank.py
@@ -0,0 +1,16 @@
+'''
+
+In a file called bank.py, implement a program that prompts the user for a greeting. If the greeting starts with “hello”, output $0. 
+If the greeting starts with an “h” (but not “hello”), output $20. Otherwise, output $100. 
+Ignore any leading whitespace in the user's greeting, and treat the user's greeting case-insensitively.
+
+'''
+
+greeting = input("Greeting: ").lstrip().lower()
+
+if greeting.startswith("hello"):
+    print("$0")
+elif greeting.startswith("h"):
+    print("$20")
+else:
+    print("$100")
