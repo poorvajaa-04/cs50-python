f3f2f09226080c3a61bed068ff8cc75146b15985
Poorvajaa <poorvajaa.16@gmail.com>
2026-08-21 10:16:39 +0530

    Implement coke problem

---
+++ week2/coke.py
@@ -0,0 +1,25 @@
+'''
+
+Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and only accepts coins in these denominations: 25 cents, 10 cents, and 
+5 cents.
+
+In a file called coke.py, implement a program that prompts the user to insert a coin, one at a time, each time informing the user of the 
+amount due. Once the user has inputted at least 50 cents, output how many cents in change the
+
+'''
+
+def main():
+    amount_due = 50
+
+    while amount_due > 0:
+        print("Amount Due:", amount_due)
+        coin = int(input("Insert Coin: "))
+
+        if coin in [25, 10, 5]:
+            amount_due -= coin
+
+    print("Change Owed:", -amount_due)
+
+
+if __name__ == "__main__":
+    main()
