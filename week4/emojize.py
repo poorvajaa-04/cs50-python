d0d5c7027a62e9cabddea9b8d3758c563703cbb3
Poorvajaa <poorvajaa.16@gmail.com>
2026-08-30 23:15:03 +0530

    Implement emoji converter program

---
+++ week4/emojize.py
@@ -0,0 +1,12 @@
+'''
+
+In a file called emojize.py, implement a program that prompts the user for a str in English and then outputs the “emojized” version of that 
+str, converting any codes (or aliases) therein to their corresponding emoji.
+
+'''
+
+import emoji
+
+text = input("Input: ")
+
+print("Output:", emoji.emojize(text, language="alias"))
