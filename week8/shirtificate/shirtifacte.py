34dbd5ad04f7a0768dcef4143a8f1d930af922cf
Poorvajaa <poorvajaa.16@gmail.com>
2026-09-05 15:55:15 +0530

    Implement code for 'shirtificate' program and create a shirtificate with my name

---
+++ week8/shirtificate/shirtifacte.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from fpdf import FPDF
+
+
+class Shirtificate:
+    def __init__(self, name):
+        self.name = name
+        self.generate()
+
+    @classmethod
+    def get(cls):
+        name = input("Name: ").strip()
+        return cls(name)
+
+    def generate(self):
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_auto_page_break(auto=False, margin=0)
+
+        pdf.set_font("Helvetica", "B", 50)
+        pdf.cell(
+            0, 50, text="CS50 Shirtificate", align="C",
+            new_x="LMARGIN", new_y="NEXT"
+        )
+
+        pdf.image(
+            Path(__file__).parent / "shirtificate.png",
+            x=5, y=80, w=200
+        )
+
+        pdf.set_font("Helvetica", "B", size=30)
+        pdf.set_text_color(255, 255, 255)
+        pdf.cell(0, 180, align="C", text=f"{self.name} took CS50")
+
+        pdf.output("shirtificate.pdf")
+
+
+def main():
+    Shirtificate.get()
+
+
+if __name__ == "__main__":
+    main()
